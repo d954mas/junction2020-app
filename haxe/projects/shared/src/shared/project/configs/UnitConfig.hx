d781d582363fe3220de89e70bb56990a1be84998
Igor Ilyin <i.ilyin@just-ai.com>
2020-11-06 22:21:38 +0300

    Deleted separate unit classes; Unified units type in single class with separate configs by UnitType

--- haxe/projects/shared/src/shared/project/configs/UnitConfig.hx
+++ haxe/projects/shared/src/shared/project/configs/UnitConfig.hx
@@ -1,37 +1,23 @@
 package shared.project.configs;
+import shared.project.enums.UnitType;
 @:expose @:keep
+typedef UnitScales = {
+var hpByLevel:Array<Int>;
+var attackByLevel:Array<Int>;
+var attackRange:Int;
+};
 class UnitConfig {
 
-    public static var ARCHER_ATTACK_RANGE = 2;
-
-    public static var attackByLevel:Array<Int> = [
-        4,
-        6,
-        9,
-        15,
-        20
-    ];
-
-    public static var archerAttackByLevel:Array<Int> = [
-        4,
-        6,
-        9,
-        15,
-        20
-    ];
-
-/*    public static var defenseByLevel:Map<Int, Int> = [
-        0,
-        1,
-        4,
-        8,
-        12
-    ];*/
-
-    public static var hpByLevel:Array<Int> = [
-        20,
-        30,
-        40,
-        80
+    public static var scalesByUnitType:haxe.ds.Map<UnitType, Dynamic> = [
+        UnitType.ARCHER => {
+            hpByLevel: [5, 10, 15, 20, 25],
+            attackByLevel: [2, 4, 6, 8, 10],
+            attackRange: 2
+        },
+        UnitType.KNIGHT => {
+            hpByLevel: [5, 10, 15, 20, 25],
+            attackByLevel: [4, 8, 12, 16, 20],
+            attackRange: 1
+        }
     ];
 }
---
+++ haxe/projects/shared/src/shared/project/enums/UnitType.hx
@@ -0,0 +1,5 @@
+package shared.project.enums;
+enum abstract UnitType(String) {
+    var ARCHER;
+    var KNIGHT;
+}
