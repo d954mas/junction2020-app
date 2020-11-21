eae3ac26c7060d8f30bc9048b385289634eaa726
d954mas <d954mas@gmail.com>
2020-11-21 23:47:39 +0300

    add x2 speed. Worked only for monetized users

--- app/models/web_monetization.lua
+++ app/models/web_monetization.lua
@@ -11,16 +11,17 @@ local COLORS = {
 local TAG = "[WEB_MONETIZATION]"
 local WebMonetization = COMMON.CLASS("WebMonetization")
 
+---@param world World
 function WebMonetization:initialize(world)
     self.world = assert(world)
     self.web_monetization = nil
 end
 
 function WebMonetization:update(dt)
-    if(SPEECH.shared) then
+    if (SPEECH.shared) then
         local web_monetization = HAXE_WRAPPER.web_monetization_is()
-        if(self.web_monetization ~= web_monetization)then
-            COMMON.i("change web_monetization:" .. tostring(web_monetization),TAG)
+        if (self.web_monetization ~= web_monetization) then
+            COMMON.i("change web_monetization:" .. tostring(web_monetization), TAG)
             self.web_monetization = web_monetization
             self:on_changed()
         end
@@ -28,10 +29,13 @@ function WebMonetization:update(dt)
 end
 
 function WebMonetization:on_changed()
-    if(COMMON.CONTEXT:exist(COMMON.CONTEXT.NAMES.MAIN_SCENE_GUI)) then
+    if (COMMON.CONTEXT:exist(COMMON.CONTEXT.NAMES.MAIN_SCENE_GUI)) then
         local ctx = COMMON.CONTEXT:set_context_top_by_name(COMMON.CONTEXT.NAMES.MAIN_SCENE_GUI)
-        local color = self.web_monetization and COLORS.ENABLE or COLORS.DISABLE
-        gui.set_color(ctx.data.vh.icon_web_monetization,color)
+        if (not self.web_monetization) then
+            --disable prem features
+            self.world:game_speed_set(1)
+        end
+        ctx.data:on_monetization_changed()
         ctx:remove()
     end
 end
--- app/models/world.lua
+++ app/models/world.lua
@@ -8,86 +8,105 @@ local WebMonetization = require "models.web_monetization"
 local World = COMMON.class("World")
 
 function World:initialize()
-	self.speech = SPEECH
-	self.intent_processor = IntentProcessor(self)
-	self.speech:set_world(self)
-	self.thread = COMMON.ThreadManager()
-	self.thread_sequence = ACTIONS.Sequence()
-	self.thread_sequence.drop_empty = false
-	self.thread:add(self.thread_sequence)
-	self.scheduler = COMMON.RX.CooperativeScheduler.create()
-	self.subscription = COMMON.EVENT_BUS:subscribe(COMMON.EVENTS.STORAGE_UPDATED):go(self.scheduler):subscribe(function()
-		self:storage_changed()
-	end)
-	self.web_monetization = WebMonetization(self)
+    self.speech = SPEECH
+    self.intent_processor = IntentProcessor(self)
+    self.speech:set_world(self)
+    self.thread = COMMON.ThreadManager()
+    self.thread_sequence = ACTIONS.Sequence()
+    self.thread_sequence.drop_empty = false
+    self.thread:add(self.thread_sequence)
+    self.scheduler = COMMON.RX.CooperativeScheduler.create()
+    self.subscription = COMMON.EVENT_BUS:subscribe(COMMON.EVENTS.STORAGE_UPDATED):go(self.scheduler):subscribe(function()
+        self:storage_changed()
+    end)
+    self.web_monetization = WebMonetization(self)
+    self.game_speed = 1
 
-	---@type nil|Level
-	self.level_model = nil
+    ---@type nil|Level
+    self.level_model = nil
 end
 
 function World:level_new()
-	print("create new level")
-	assert(not self.level_model)
-	self.level_model = Level(self)
+    print("create new level")
+    assert(not self.level_model)
+    self.level_model = Level(self)
 end
 
 function World:level_restart()
-	self.level_model = nil
+    self.level_model = nil
+end
+
+function World:game_speed_set(speed)
+    self.game_speed = assert(speed)
+end
+
+function World:game_speed_get()
+    return self.game_speed
+end
+
+function World:game_speed_next()
+    local speed = self:game_speed_get()
+    if (speed == 1) then
+        self:game_speed_set(1.5)
+    elseif (speed == 1.5) then
+        self:game_speed_set(2)
+    else
+        self:game_speed_set(1)
+    end
 end
 
 function World:update(dt)
-	self.web_monetization:update(dt)
-	if(self.level_model)then
-		self.level_model:update(dt)
-	end
+    dt = dt * self.game_speed
+    self.web_monetization:update(dt)
+    if (self.level_model) then
+        self.level_model:update(dt)
+    end
 
-	self.thread:update(dt)
-	self.scheduler:update(dt)
+    self.thread:update(dt)
+    self.scheduler:update(dt)
 end
 
 function World:storage_changed()
-	if(self.level_model)then
-		self.level_model:storage_changed()
-	end
+    if (self.level_model) then
+        self.level_model:storage_changed()
+    end
 end
 --endregion
 
 
 function World:speech_process_intent(msg)
-	self.intent_processor:process(msg)
+    self.intent_processor:process(msg)
 end
 
 function World:castle_idx_to_position(idx)
-	local pos_x = COMMON.CONSTANTS.CONFIG.CASTLE_PAD_BORDER + COMMON.CONSTANTS.CONFIG.CASTLE_SIZE/2
-	if(idx == 0) then
-		pos_x = pos_x + 230
-	end
-	pos_x = pos_x + (1280-COMMON.CONSTANTS.CONFIG.CASTLE_PAD_BORDER*2-COMMON.CONSTANTS.CONFIG.CASTLE_SIZE)/2*idx
-	return vmath.vector3(pos_x,270,-0.8)
+    local pos_x = COMMON.CONSTANTS.CONFIG.CASTLE_PAD_BORDER + COMMON.CONSTANTS.CONFIG.CASTLE_SIZE / 2
+    if (idx == 0) then
+        pos_x = pos_x + 230
+    end
+    pos_x = pos_x + (1280 - COMMON.CONSTANTS.CONFIG.CASTLE_PAD_BORDER * 2 - COMMON.CONSTANTS.CONFIG.CASTLE_SIZE) / 2 * idx
+    return vmath.vector3(pos_x, 270, -0.8)
 end
 
-function World:road_idx_to_position(road_idx,road_part_idx)
-	local y = 270
-	local castle_pos = self:castle_idx_to_position(road_idx).x
-	local castle_pos_next = self:castle_idx_to_position(road_idx+1).x
-	local dmove = castle_pos_next-castle_pos-COMMON.CONSTANTS.CONFIG.CASTLE_SIZE-COMMON.CONSTANTS.CONFIG.ROAD_CASTLES_PAD*2
-	local roads_per_move = 5
-	if(road_idx==0)then
-		roads_per_move = 2
-	end
+function World:road_idx_to_position(road_idx, road_part_idx)
+    local y = 270
+    local castle_pos = self:castle_idx_to_position(road_idx).x
+    local castle_pos_next = self:castle_idx_to_position(road_idx + 1).x
+    local dmove = castle_pos_next - castle_pos - COMMON.CONSTANTS.CONFIG.CASTLE_SIZE - COMMON.CONSTANTS.CONFIG.ROAD_CASTLES_PAD * 2
+    local roads_per_move = 5
+    if (road_idx == 0) then
+        roads_per_move = 2
+    end
 
+    local cell_size = dmove / roads_per_move
 
+    pprint(cell_size)
 
-	local cell_size = dmove/roads_per_move
+    local road_pos = castle_pos + COMMON.CONSTANTS.CONFIG.ROAD_CASTLES_PAD + COMMON.CONSTANTS.CONFIG.CASTLE_SIZE / 2
+    road_pos = road_pos + cell_size * road_part_idx + COMMON.CONSTANTS.CONFIG.ROAD_SIZE / 2
 
-	pprint(cell_size)
+    road_pos = road_pos - cell_size
 
-	local road_pos = castle_pos + COMMON.CONSTANTS.CONFIG.ROAD_CASTLES_PAD + COMMON.CONSTANTS.CONFIG.CASTLE_SIZE/2
-	road_pos = road_pos + cell_size*road_part_idx + COMMON.CONSTANTS.CONFIG.ROAD_SIZE/2
-
-	road_pos = road_pos - cell_size
-
-	return vmath.vector3(road_pos,y,-0.9)
+    return vmath.vector3(road_pos, y, -0.9)
 
 end
 
